81ca59ce3a08abf6e9f685ba5580d4ebd4b7b208
Dmitriy <pointsman1@gmail.com>
2021-11-17 21:24:33 +0200

    styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -20,9 +20,83 @@ export default function Navbar({}) {
   return (
     <>
       <div className="navbar shadow-lg bg-neutral text-neutral-content rounded-box mb-6">
+        <div className=" px-2 mx-2 ">
+          <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
+              />
+            </svg>
+            Rewriter
+          </Link>
+        </div>
+        <div className=" px-2 mx-2">
+          <Link to="/info/about" className="btn btn-ghost btn-sm rounded-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+              />
+            </svg>
+            About
+          </Link>
+        </div>
+        <div className=" px-2 mx-2">
+          <Link to="/info/api" className="btn btn-ghost btn-sm rounded-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+            API
+          </Link>
+        </div>
         <div className="flex-1 px-2 mx-2">
-          <Link to="/" className="text-lg font-bold">
-            Dofiltra
+          <Link
+            to="/info/contacts"
+            className="btn btn-ghost btn-sm rounded-btn"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+              />
+            </svg>
+            Contacts
           </Link>
         </div>
         <div className="flex-none">
@@ -55,15 +129,22 @@ export default function Navbar({}) {
                 <div tabIndex={0} className="btn btn-ghost rounded-btn">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
                     fill="none"
                     viewBox="0 0 24 24"
-                    className="inline-block w-6 h-6 stroke-current"
+                    stroke="currentColor"
                   >
                     <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16M4 18h16"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                     />
                   </svg>
                 </div>
--- src/views/rewriter/Result.tsx
+++ src/views/rewriter/Result.tsx
@@ -13,12 +13,12 @@ type TRewriterResultPage = {
 function getBackgroundColorByStatus(status: number) {
   switch (status) {
     case 3: // inProgrss
-      return '#e1f5fe'
+      return 'alert-info'
     case 9: // completed
-      return '#dcedc8'
+      return 'alert-success'
   }
 
-  return '#e0e0e0'
+  return ''
 }
 
 const RewriterResultPage: FC<TRewriterResultPage> = () => {
@@ -57,38 +57,49 @@ const RewriterResultPage: FC<TRewriterResultPage> = () => {
 
   return (
     <>
-      <div className="min-h-full">
+      <div className="min-h-full w-full card bg-base-200">
         <main>
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             <div className="px-4 sm:px-0">
               <div className="mb-1 md:mb-0 w-full p-1">
-                <label
-                  className="text-gray-700 p-2"
-                  style={{
-                    backgroundColor: getBackgroundColorByStatus(
-                      rewriteData.status
-                    ),
-                  }}
-                >
-                  {'TaskStatus'}:{' '}
-                  {getRewriterStatusText(rewriteData.status).toLowerCase()} (
-                  {isCompleted
-                    ? '100.00'
-                    : (
-                        (blocksRewrited.length / blocksForRewrite.length) *
-                        100
-                      ).toFixed(2)}
-                  %)
-                </label>
+                <div className={'alert ' + getBackgroundColorByStatus(status)}>
+                  <div className="flex-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="w-6 h-6 mx-2 stroke-current"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                      />
+                    </svg>
+                    <label>
+                      {'TaskStatus'}:{' '}
+                      {getRewriterStatusText(rewriteData.status).toLowerCase()}{' '}
+                      (
+                      {isCompleted
+                        ? '100.00'
+                        : (
+                            (blocksRewrited.length / blocksForRewrite.length) *
+                            100
+                          ).toFixed(2)}
+                      %)
+                    </label>
+                  </div>
+                </div>
               </div>
 
               <div className="mb-1 md:mb-0 w-full p-2">
-                <label className="text-gray-700">{'SelectedTargetLang'}</label>
+                <label className="text-white">{'SelectedTargetLang'}</label>
                 <select
                   name="targetLang"
                   value={rewriteData.targetLang}
                   disabled
-                  className="h-10 pl-3 pr-6 text-base w-full placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                  className="select select-bordered select-warning w-full"
                 >
                   <option value={1}>Russian</option>
                   <option value={0}>English</option>
@@ -96,16 +107,16 @@ const RewriterResultPage: FC<TRewriterResultPage> = () => {
               </div>
 
               <div className="grid grid-cols-2 gap-1">
-                <div className="mb-1 md:mb-0 w-full p-2 card bg-base-200">
-                  <label className="text-gray-700">{'Original text'}</label>
+                <div className="mb-1 md:mb-0 w-full p-2 ">
+                  <label className="text-white">{'Original text'}</label>
                   <div className="editor-wrapper w-full border-4 border-dashed border-gray-200 rounded-lg p-3">
                     <ReactEditor holder="orig" readOnly={true} data={data} />
                   </div>
                 </div>
 
                 {isShowResult && (
-                  <div className="mb-1 md:mb-0 w-full p-2 card bg-base-200">
-                    <label className="text-gray-700">{'Rewrited text'}</label>
+                  <div className="mb-1 md:mb-0 w-full p-2 ">
+                    <label className="text-white">{'Rewrited text'}</label>
                     <div className="editor-wrapper w-full border-4 border-dashed border-gray-200 rounded-lg p-3">
                       <ReactEditor holder="rewrite" data={dataRewrite} />
                     </div>
