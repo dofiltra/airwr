9c4cc75348e4e44bf00ac29555ee164f062243b8
Dmitrii Osipovich <menemty@gmail.com>
2022-01-18 09:28:48 +0200

    .

--- src/views/extractor/Extractor.tsx
+++ src/views/extractor/Extractor.tsx
@@ -48,7 +48,17 @@ export default () => {
               {translate('ExtractorTitle')}
               <img src={smileSrc} className="inline px-4" />
             </h1>
-            <p className="w-full pt-6">{translate('ExtractorText')}</p>
+          </div>
+
+          <div className="w-full card p-4">
+            <div className="mb-1 md:mb-0 w-full p-2 ">
+              <label className="">{translate('EnterTextForExtractor')}</label>
+              <textarea
+                className="editor-wrapper w-full border-4 border-dashed border-gray-200 rounded-lg p-3"
+                rows={10}
+                placeholder={`${translate('EnterTextForExtractorPlaceholder')}`}
+              ></textarea>
+            </div>
           </div>
         </main>
       </div>
