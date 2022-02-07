f4a30d566053eb1fabbdde30511f5f9a5fc3d56e
Dmitrii Osipovich <menemty@gmail.com>
2022-02-07 10:34:19 +0200

    .

--- src/views/odm/stats.tsx
+++ src/views/odm/stats.tsx
@@ -5,13 +5,14 @@
 
 import { FC } from 'preact/compat'
 import { HOST_API } from 'helpers/api'
-import { SocketEvent } from 'dprx-types'
+import { ProxyItem, SocketEvent } from 'dprx-types'
 import { io } from 'socket.io-client'
 import { useEffect, useState } from 'preact/hooks'
+import _ from 'lodash'
 
 export const OdmStatsPage: FC = () => {
-  const [data, setData] = useState({} as any)
-  const [proxies, setProxies] = useState({} as any)
+  const [socketsData, setSocketsData] = useState([] as any[])
+  const [proxies, setProxies] = useState([] as any[])
 
   useEffect(() => {
     fetch(`${HOST_API}/api/socketio/exec`).finally(() => {
@@ -25,9 +26,9 @@ export const OdmStatsPage: FC = () => {
       })
 
       socket.on(SocketEvent.OdmStats, ({ socketsData, used }: any) => {
-        setData(socketsData)
-        setProxies(used)
-        console.log(socketsData, used)
+        setSocketsData(socketsData)
+        setProxies(_.uniq(used))
+        // console.log(socketsData, used)
       })
     })
   }, [])
@@ -38,11 +39,20 @@ export const OdmStatsPage: FC = () => {
         <div className="mb-1 md:mb-0 w-full p-2 ">
           <label>Sockets data</label>
           <pre className="editor-wrapper w-full border-4 border-dashed border-gray-200 rounded-lg p-3 overflow-auto">
-            {JSON.stringify(data, null, 2)}
+            {JSON.stringify(
+              socketsData?.map((dataKey: any) => ({
+                ...dataKey,
+                proxies: dataKey?.proxies
+                  ?.map((p: ProxyItem) => `${p?.ip}:${p?.port}`)
+                  .join('; '),
+              })),
+              null,
+              2
+            )}
           </pre>
         </div>
         <div className="mb-1 md:mb-0 w-full p-2 ">
-          <label>Proxies</label>
+          <label>Proxies ({proxies?.length || 0})</label>
           <div className="editor-wrapper w-full border-4 border-dashed border-gray-200 rounded-lg p-3 min-h-16">
             {JSON.stringify(proxies, null, 2)}
           </div>
