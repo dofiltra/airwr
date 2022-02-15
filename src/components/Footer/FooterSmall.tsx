be21aa02317e4ebd6938f9ae35161a887bf6c0b0
dofitra <menemty@gmail.com>
2022-02-15 18:20:32 +0000

    .

--- src/components/Footer/FooterSmall.tsx
+++ src/components/Footer/FooterSmall.tsx
@@ -22,22 +22,22 @@ export default function FooterSmall() {
             </div>
             <div className="w-full md:w-8/12 px-4">
               <ul className="flex flex-wrap list-none md:justify-end  justify-center">
-                <li className="bordered border-r-2 px-4">
+                {/* <li className="bordered border-r-2 px-4">
                   <Link
                     to="/info/about"
                     className="hover:text-gray-400 text-sm font-semibold block py-1"
                   >
                     About
                   </Link>
-                </li>
-                <li className="bordered border-r-2 px-4">
+                </li> */}
+                {/* <li className="bordered border-r-2 px-4">
                   <Link
                     to="/info/contacts"
                     className="hover:text-gray-400 text-sm font-semibold block py-1"
                   >
                     Contacts
                   </Link>
-                </li>
+                </li> */}
                 <li>
                   <a
                     href="https://t.me/dofiltra"
