390e6537fc185d6b5904953ca095423264e797c5
Dmitrii Osipovich <menemty@gmail.com>
2022-03-29 09:56:11 +0300

    ю

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { FooterSmall, Root } from '@dofiltra/tailwind'
+import { useLocalize } from '@borodutch-labs/localize-react'
 import AuthContextProvider from 'components/Auth/AuthContextProvider'
 import LocalizationProvider from 'localization/LocalizationProvider'
 import Navbar from 'components/Navbar/Navbar'
@@ -20,7 +21,19 @@ export const App = ({
                 return <Route path={route.path} element={route.element} />
               })}
             </Routes>
-            {/* <FooterSmall siteName={'Dofiltra'} contactsText={'Contacts'} /> */}
+            <FooterSmall
+              siteName={'Dofiltra'}
+              serviceItems={[
+                <li className="bordered border-r-0 px-4">
+                  <Link
+                    to="/info/contacts"
+                    className="hover:text-gray-400 text-sm font-semibold block py-1"
+                  >
+                    {'Contacts'}
+                  </Link>
+                </li>,
+              ]}
+            />
           </AuthContextProvider>
         </LocalizationProvider>
       </Root>
